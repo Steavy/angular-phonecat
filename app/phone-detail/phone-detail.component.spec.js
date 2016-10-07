3945e4c5eed89a823ccf27a1cc71d2b2c64dee06
Steven Nienhuis <djsteavy@gmail.com>
2016-10-07 10:15:49 +0200

    negatieve test voor phone-detail toegevoegd

--- app/phone-detail/phone-detail.component.spec.js
+++ app/phone-detail/phone-detail.component.spec.js
@@ -5,7 +5,7 @@ describe('phoneDetail', function() {
   // Load the module that contains the `phoneDetail` component before each test
   beforeEach(module('phoneDetail'));
 
-  // Test the controller
+  // Test the controller with a positive test
   describe('PhoneDetailController', function() {
     var $httpBackend, ctrl;
     var xyzPhoneData = {
@@ -22,7 +22,7 @@ describe('phoneDetail', function() {
       ctrl = $componentController('phoneDetail');
     }));
 
-    it('should fetch the phone details', function() {
+    it('should fetch the XYZ phone details', function() {
       jasmine.addCustomEqualityTester(angular.equals);
 
       expect(ctrl.phone).toEqual({});
@@ -32,5 +32,36 @@ describe('phoneDetail', function() {
     });
 
   });
+
+  // Test the controller with a negative test
+    describe('PhoneDetailController', function() {
+      var $httpBackend, ctrl;
+      var xyzPhoneData = {
+            name: 'phone xyz',
+            images: ['image/url1.png', 'image/url2.png']}
+      var abcPhoneData = {
+        name: 'phone abc',
+        images: ['image/url2.png', 'image/url4.png']
+      };
+
+      beforeEach(inject(function($componentController, _$httpBackend_, $routeParams) {
+        $httpBackend = _$httpBackend_;
+        $httpBackend.expectGET('phones/abc.json').respond(abcPhoneData);
+
+        $routeParams.phoneId = 'abc';
+
+        ctrl = $componentController('phoneDetail');
+      }));
+
+      it('should NOT fetch the XYZ phone details with ABC phone details', function() {
+        jasmine.addCustomEqualityTester(angular.equals);
+
+        expect(ctrl.phone).toEqual({});
+
+        $httpBackend.flush();
+        expect(ctrl.phone).not.toEqual(xyzPhoneData);
+      });
+
+    });
 
 });
